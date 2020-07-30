fef80b6b9bc4f36c3c12d1f12d4906f218d645d5
optitech-games <olofakerfeldt@hotmail.com>
2020-07-30 18:06:37 +0200

    initial

---
+++ KorvMackaMedOst/Data/Migrations/20200726145101_add-todoitem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KorvMackaMedOst.Data.Migrations
+{
+    public partial class addtodoitem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<string>(nullable: false),
+                    ComlpetedDate = table.Column<DateTime>(nullable: false),
+                    IsCompleted = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItems", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItems");
+        }
+    }
+}
---
+++ KorvMackaMedOst/Models/TodoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KorvMackaMedOst.Models
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+        public string UserId { get; set; }
+        public DateTime ComlpetedDate { get; set; }
+        public bool IsCompleted { get; set; }
+        [Required]
+        public string Content { get; set; }
+    }
+}
